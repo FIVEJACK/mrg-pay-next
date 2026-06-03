ec841d0b2a648b5e1a4dce3b1e867686449f8e2b
stevenlo12 <58546250+stevenlo12@users.noreply.github.com>
2026-06-03 09:55:53 +0700

    replace lg icon to mrg icon

--- src/components/lapakgaming/brand-logo.tsx
+++ src/components/lapakgaming/brand-logo.tsx
@@ -1,12 +1,7 @@
+import { MultiRealmIcon } from "@/components/icon";
+
 export function BrandLogo({ className }: { className?: string }) {
   return (
-    // eslint-disable-next-line @next/next/no-img-element
-    <img
-      src="/lapakgaming-logo.svg"
-      alt="LapakGaming"
-      width={160}
-      height={36}
-      className={`block h-9 w-auto ${className ?? ""}`}
-    />
+    <MultiRealmIcon className={`block h-9 w-auto text-(--color-text-title) ${className ?? ""}`} />
   );
 }
